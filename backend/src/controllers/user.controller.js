4e3196da002c547cec98e5dd02c3f55b29c62d5b
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-09-04 14:02:18 +0500

    implement user authentication with access and refresh tokens; add ApiError and ApiResponse classes for error handling

--- backend/src/controllers/user.controller.js
+++ backend/src/controllers/user.controller.js
@@ -2,13 +2,29 @@ import { User } from '../models/user.model.js';
 import asyncHandler from 'express-async-handler';
 import bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken'
+import {ApiError} from '../utils/api-error.js';
+import {ApiResponse} from '../utils/api-response.js'
+
+const generateAccessTokenAndRefreshToken = async(userId) => {
+  try {
+    const user = await User.findById(userId);
+
+    const accessToken = user.generateAccessToken();
+    const refreshToken = user.generateRefreshToken();
+
+    user.refreshToken = refreshToken;
+    await user.save({validateBeforeSave: false});
+    return {accessToken, refreshToken};
+  } catch (error) {
+    throw new ApiError(404, "User not found")
+  }
+}
 
 const registerUser = asyncHandler(async (req, res) => {
   const { name, email, password } = req.body;
 
   if (!name || !email || !password) {
-    res.status(400);
-    throw new Error('Please provide all required fields');
+    throw new ApiError(400,'Please provide all required fields');
   }
 
   const userExists = await User.findOne({ email });
@@ -26,14 +42,17 @@ const registerUser = asyncHandler(async (req, res) => {
     password: hashedPassword,
   })
 
+  const {accessToken, refresToken} = await generateAccessTokenAndRefreshToken(newUser._id);
+
+  const options = {
+    httpOnly: true,
+    secure: true
+  }
+
   if (newUser) {
-    res.status(201).json({
-      _id: newUser._id,
-      name: newUser.name,
-      email: newUser.email,
-      password: newUser.password,
-      token: jwt.sign({ _id: newUser._id }, process.env.JWT_SECRET, { expiresIn: "1h" }),
-    })
+    res.status(201).cookie("accessToken", accessToken, options).cookie("refreshToken", refreshToken, options).json(
+      new ApiResponse(201, {newUser}, "New User created Successfully")
+    )
   } else {
     res.status(400).json({ message: "Could not created user" });
   }
--- backend/src/models/user.model.js
+++ backend/src/models/user.model.js
@@ -1,5 +1,6 @@
 import mongoose from 'mongoose';
-
+import jwt from 'jsonwebtoken';
+import bcrypt from 'bcrypt';
 const userSchema = new mongoose.Schema({
   name: {
     type: String,
@@ -14,6 +15,18 @@ const userSchema = new mongoose.Schema({
     required: true,
   },
 });
+
+userSchema.methods.isPasswordValid = async function (password) {
+  return await bcrypt.compare(password, this.password)
+}
+
+userSchema.methods.generateAccessToken = function() {
+  return jwt.sign({_id: this._id}, process.env.ACCESS_TOKEN_SECRET, {expiresIn: process.env.ACCESS_TOKEN_EXPIRY})
+}
+
+userSchema.methods.generateRefreshToken= function(){
+  return jwt.sign({_id: this._id}, process.env.REFRESH_TOKEN_SECRET, {expiresIn: process.env.REFRESH_TOKEN_EXPIRY})
+}
 
 
 export const User = mongoose.model("User", userSchema);
