890d086114e8a750c6cd384eb34dfb1be0db3235
Abbas-shah10 <abbaskhanshah10@gmail.com>
2026-09-10 21:37:44 +0500

    added the auth routes

--- backend/src/app.js
+++ backend/src/app.js
@@ -20,7 +20,10 @@ app.use(cors({
 
 // import routes
 import healthRouter from './routes/healthcheck.routes.js'
+import authRouter from './routes/auth.routes.js';
+
 
 app.use('/api/v1/healthcheck', healthRouter)
+app.use('/api/v1/auth', authRouter)
 
 export default app;
